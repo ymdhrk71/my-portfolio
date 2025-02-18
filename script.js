1062c14fa817f5c1d48f971cc2746755248b3786
yamada <haruki.yamada71@gmail.com>
2025-02-18 22:17:43 +0900

    #

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+window.addEventListener ('load', () => {
+  // 3秒間ボールをバウンドさせる
+  setTimeout (() => {
+    // 読み込み画面を非表示にする
+    const loader = document.querySelector ('.loader');
+    loader.style.opacity = '0'; // フェードアウト
+    loader.style.pointerEvents = 'none'; // ユーザーが操作できないようにする
+
+    // Chromeウィンドウを拡大し、定位置に移動する
+    const chromeWindow = document.querySelector ('.chrome-window');
+    chromeWindow.classList.add ('show');
+  }, 2000);
+});
+
 document.addEventListener ('DOMContentLoaded', () => {
   const tabButtons = document.querySelectorAll ('.tab-button');
   const tabContents = document.querySelectorAll ('.tab-content');
